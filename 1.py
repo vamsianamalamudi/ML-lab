2de482a4e5f1cd6416bbcc80512135cba73704cf
Vamsi <37373043+vamsianamalamudi@users.noreply.github.com>
2019-12-09 19:12:36 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,1 +1,21 @@
-
+#program1 findS
+import csv 
+with open('prg1.csv','r') as f :
+    reader = csv.reader(f)
+    your_list = list(reader)
+h = [['0','0','0','0','0','0']]
+for i in your_list :    
+    print(i)    
+    if i[-1] == "yes":
+        j = 0
+        for x in i:
+            if x != "yes":
+                if x != h[0][j] and h[0][j] == '0':
+                    h[0][j] = x
+                elif x != h[0][j] and h[0][j] != '0':
+                    h[0][j] = '?'
+                else:
+                    pass                
+                j = j + 1
+print("most specfic hypothesis is")    
+print (h)
